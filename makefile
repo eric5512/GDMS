8e2b80a2275f209e1edfde54eed19de0ccb938d8
Eric <ericdandria2@gmail.com>
2023-09-03 19:54:34 +0200

    First commit

---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -Wall -Wextra -Werror src/main.c -o build/main.exe
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <argp.h>
+
+int main(int argc, char const *argv[]) {
+    (void) argc;
+    (void) argv;
+
+    printf("Hello world\n");
+    
+    return 0;
+}
